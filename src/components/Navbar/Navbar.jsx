48c8c44c7499e2b3e974663ddbade011e6eac7eb
Sheikhmsip <sheikhmsip@gmail.com>
2026-03-04 18:46:21 +0600

    project initialization and with navbar design

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Navbar = () => {
+
+  const navItems = <>
+    <li><a>Home</a></li>
+    <li>
+      <a href="">FAQ</a>
+    </li>
+    <li><a>Changelog</a></li>
+    <li><a>Blog</a></li>
+    <li><a>Download</a></li>
+    <li><a>Contact</a></li></>
+  return (
+    <div className='bg-white text-black'>
+      <div className="navbar shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><a>Home</a></li>
+              <li>
+                <a href="">FAQ</a>
+              </li>
+              <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl"> Ticket Managment </a>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              {navItems}
+            </ul>
+          </div>
+          <div className="btn font-semibold text-[16px] bg-gradient-to-r from-[#632EE3] to-[#9F62F2]">
+            <span >+</span>
+            <span >New Ticket</span>
+          </div>
+          
+      </div>
+    </div>
+        </div >
+    );
+};
+
+export default Navbar;
