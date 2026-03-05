98a84898135ff872ad2a7917d7235c6fbef5d968
Sheikhmsip <sheikhmsip@gmail.com>
2026-03-05 17:45:00 +0600

    load tickets data and show in card

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,31 @@ import CountTask from './components/CountTask/CountTask'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Fotter'
 import CustomerTicket from './components/CustomerTicket/CustomerTicket'
+import { Suspense } from 'react'
 
 
-
+const fatchTickets = async () => {
+  try {
+    const response = await fetch('/tickets.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching tickets:', error);
+    throw error;
+  }
+};
 
 function App() {
+
+  const ticketsPromise = fatchTickets();
   return (
     <div className='bg-white w-full'>
       <Navbar></Navbar>
       <CountTask></CountTask>
-      
-        <CustomerTicket  />
+      <Suspense fallback={<div className='text-center text-2xl text-[#34485A] font-semibold'>Loading Tickets...</div>}>
+
+        <CustomerTicket ticketsPromise={ticketsPromise}></CustomerTicket>
+      </Suspense>
      
       <Footer></Footer>
     </div>
--- src/components/CustomerTicket/CustomerTicket.jsx
+++ src/components/CustomerTicket/CustomerTicket.jsx
@@ -1,11 +1,13 @@
+import React, { use } from 'react';
 import CustomerTicketCard from './CustomerTicketCard';
 import TaskStatus from './TaskStatus';
 
-const CustomerTicket = () => {
-    
+const CustomerTicket = ({ticketsPromise}) => {
+    const ticketsData = use(ticketsPromise);
+    console.log(ticketsData);
     return (
         <div className='md:w-10/12 mx-auto mt-5 gap-5 px-2 flex flex-col-reverse md:flex items-start justify-start lg:flex-row '>
-            <CustomerTicketCard></CustomerTicketCard>
+            <CustomerTicketCard tickets={ticketsData}></CustomerTicketCard>
             <TaskStatus></TaskStatus>
         </div>
     );
